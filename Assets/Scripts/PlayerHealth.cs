e78e73132dbec9fa089d4b21ea437e7c86a4463b
username-new-world <isthisrintan@gmail.com>
2026-06-30 20:47:33 +0330

    feat: add health bar

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,23 +3,25 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int maxHealth = 100;
+    [SerializeField] HealthBar healthBar;
 
     int currentHealth;
 
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-
-        Debug.Log("Player HP: " + currentHealth);
+        healthBar.SetHealth(currentHealth);
 
         if(currentHealth <= 0)
         {
             Debug.Log("Player Dead");
+
         }
     }
 }
